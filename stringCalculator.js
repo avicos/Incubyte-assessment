2e7ccb4a9b85969202cdc7a339266f954419878b
avicos <adapaavinash612@gmail.com>
2024-10-19 11:24:41 +0530

    Code to pass the third test

--- stringCalculator.js
+++ stringCalculator.js
@@ -5,6 +5,18 @@ function add(numbers) {
     if(numbers.length === 1) {
         return parseInt(numbers);
     }
+
+    const numberList = numbers.split(',');
+
+    let total = 0;
+    for (let num of numberList) {
+        if (num) {
+            total += parseInt(num, 10);
+        }
+    }
+
+    return total;
+
 }
 
 module.exports = add;
