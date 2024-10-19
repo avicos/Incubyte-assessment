277bec6d76eeb5fde93edc026c4432664156abfd
avicos <adapaavinash612@gmail.com>
2024-10-19 11:18:51 +0530

    Create second test

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -3,3 +3,7 @@ const addTest = require('./stringCalculator');
 test('should return 0 for an empty string', () => {
     expect(addTest("")).toBe(0);
 });
+
+test('should return the number itself when one number is passed', () => {
+    expect(addTest("5")).toBe(5);
+});
