1a7f8e1f28dc395ced799a1ac921c4f8c507b795
avicos <adapaavinash612@gmail.com>
2024-10-19 12:05:21 +0530

    Code to pass custom delimitor tests and refactoring

--- stringCalculator.js
+++ stringCalculator.js
@@ -3,22 +3,51 @@ function add(numbers) {
         return 0;
     }
 
-    numbers = numbers.replace(/\n/g, ',').replace(/;/g, ',');
- 
-    const numberList = numbers.split(',');
+    let delimiters = [',', '\n', ';'];  
+
+    if (numbers.startsWith('//')) {
+        const delimiterEndIndex = numbers.indexOf('\n');
+        let customDelimiter = numbers.substring(2, delimiterEndIndex);
+
+        delimiters.push(customDelimiter);
+        numbers = numbers.substring(delimiterEndIndex + 1);
+    }
+
+    let numberList = splitByDelimiters(numbers, delimiters);
 
     let total = 0;
+    let negativeNumbers = [];
+
     for (let num of numberList) {
-        if(num < 0) {
-            throw new Error("Negative numbers not allowed: " + num);
-        }
         if (num) {
-            total += parseInt(num, 10);
+            let number = parseInt(num, 10);
+            if (number < 0) {
+                negativeNumbers.push(number);
+            } else {
+                total += number;
+            }
         }
     }
 
-    return total;
+    if (negativeNumbers.length > 0) {
+        throw new Error(`Negative numbers not allowed: ${negativeNumbers.join(', ')}`);
+    }
 
+    return total;
+}
+
+function splitByDelimiters(string, delimiters) {
+    let result = [string];
+    
+    for (let delimiter of delimiters) {
+        let tempResult = [];
+        for (let part of result) {
+            tempResult = tempResult.concat(part.split(delimiter));
+        }
+        result = tempResult;
+    }
+    
+    return result;
 }
 
 module.exports = add;
